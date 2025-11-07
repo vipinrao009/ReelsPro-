ce32cb15afbc1a8fa6cb6787b505e35c71358766
vipinrao009 <vipinrao2018@gmail.com>
2025-11-07 16:29:42 +0530

    Add bcryptjs for password hashing and implement User and Video models with Mongoose

---
+++ models/Video.ts
@@ -0,0 +1,67 @@
+import mongoose, { model, models, Schema } from "mongoose";
+
+export const VIDEO_DIMENTIONS = {
+  width: 1080,
+  height: 1920,
+} as const;
+
+export interface IVideo {
+  _id: mongoose.Types.ObjectId;
+  title: string;
+  description: string;
+  videoUrl: string;
+  thumbnailUrl: string;
+  controls?: boolean;
+  transformation?: {
+    height: number;
+    width: number;
+    quality?: number;
+  };
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const videoSchema = new Schema<IVideo>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    videoUrl: {
+      type: String,
+      required: true,
+    },
+    thumbnailUrl: {
+      type: String,
+      required: true,
+    },
+    controls: {
+      type: Boolean,
+      default: true,
+    },
+    transformation: {
+      height: {
+        type: Number,
+        default: VIDEO_DIMENTIONS.height,
+      },
+      width: {
+        type: Number,
+        default: VIDEO_DIMENTIONS.width,
+      },
+      quality: {
+        type: Number,
+        min: 1,
+        max: 100,
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+const Video =models?.User || model<IVideo>("Video", videoSchema)
+
+export default Video
